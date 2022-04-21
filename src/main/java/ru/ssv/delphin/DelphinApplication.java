e6722e23c60681a41d65ee702f427589ce06d54d
17866673 <shcheglov.s.vik@sberbank.ru>
2022-04-21 16:15:19 +0300

    init commit

---
+++ src/main/java/ru/ssv/delphin/DelphinApplication.java
@@ -0,0 +1,13 @@
+package ru.ssv.delphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelphinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelphinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ssv/delphin/DelphinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssv.delphin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelphinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
